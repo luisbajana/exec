8f701cf6e067b03e6a79a948ef29f94a10864df1
Luis Bajana <lbajana@gmail.com>
2013-10-30 20:56:16 -0500

    ´

---
+++ exec.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+
+	char comando[100];
+	printf("Ingresa un comando: \n");
+	scanf("%s",comando);
+
+	/*
+		This system call, creates a brand new proccess, the first argument is the program that I want to execute, the second are the
+		parameters to that program, and the third the enviorment variables.
+	*/
+	execl("./a.out",NULL,NULL);
+
+	return 0;
+}
